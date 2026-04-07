bb32c685895d82a3f2438a74a817e36cfce99d35
faltu99 <forfaltuuse09@gmail.com>
2026-04-07 15:04:17 +0600

    Update role.js

--- src/commands/Moderation/role.js
+++ src/commands/Moderation/role.js
@@ -8,14 +8,12 @@ export default {
         .setName("role")
         .setDescription("Manage user roles")
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
-        // Subcommand: ADD
         .addSubcommand(sub =>
             sub.setName("add")
                 .setDescription("Add a role to a member")
                 .addUserOption(o => o.setName("target").setDescription("The member").setRequired(true))
                 .addRoleOption(o => o.setName("role").setDescription("The role to add").setRequired(true))
         )
-        // Subcommand: REMOVE
         .addSubcommand(sub =>
             sub.setName("remove")
                 .setDescription("Remove a role from a member")
@@ -37,13 +35,12 @@ export default {
                 throw new Error("That user is not in this server.");
             }
 
-            // Check if the bot can actually manage this role
             if (role.managed) {
-                throw new Error("I cannot manage this role because it is controlled by an integration (like another bot).");
+                throw new Error("I cannot manage this role because it is controlled by an integration.");
             }
 
             if (interaction.guild.members.me.roles.highest.position <= role.position) {
-                throw new Error("I cannot manage this role because it is higher than or equal to my own role in the hierarchy.");
+                throw new Error("I cannot manage this role because it is higher than mine.");
             }
 
             if (sub === "add") {
@@ -51,8 +48,10 @@ export default {
                     throw new Error(`${target.user.tag} already has that role.`);
                 }
                 await target.roles.add(role);
+                
+                // Using role.name instead of just role
                 await InteractionHelper.safeEditReply(interaction, {
-                    embeds: [successEmbed("Role Added", `Successfully added ${role} to ${target}`)]
+                    embeds: [successEmbed("Role Added", `Successfully added the **${role.name}** role to ${target}`)]
                 });
             } 
             
@@ -61,8 +60,10 @@ export default {
                     throw new Error(`${target.user.tag} does not have that role.`);
                 }
                 await target.roles.remove(role);
+
+                // Using role.name instead of just role
                 await InteractionHelper.safeEditReply(interaction, {
-                    embeds: [successEmbed("Role Removed", `Successfully removed ${role} from ${target}`)]
+                    embeds: [successEmbed("Role Removed", `Successfully removed the **${role.name}** role from ${target}`)]
                 });
             }
 
@@ -74,4 +75,3 @@ export default {
         }
     }
 };
-                  
